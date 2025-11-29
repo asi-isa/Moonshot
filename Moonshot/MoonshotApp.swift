bfe796f55c127b7be5e5854de99a20c2f5aaec26
asi-isa <ali.inceoglu@protonmail.com>
2025-11-29 13:08:37 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Ali Soner Inceoglu on 29.11.25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
